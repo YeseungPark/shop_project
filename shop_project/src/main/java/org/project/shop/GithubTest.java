a61533105c7c3328769a96ed0a08156413c14328
pys <avi09@hanmail.net>
2017-01-11 12:22:25 +0900

    hi

--- shop_project/src/main/java/org/project/shop/GithubTest.java
+++ shop_project/src/main/java/org/project/shop/GithubTest.java
@@ -5,6 +5,7 @@ public class GithubTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi gitHuB!");
+		System.out.println("hi miku");
 	}
 
 }

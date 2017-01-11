6ccd6a0b0bf6f8ce6fe944d764977dd3d30b88c5
pys <avi09@hanmail.net>
2017-01-11 12:19:41 +0900

    my first github test

---
+++ shop_project/src/main/java/org/project/shop/GithubTest.java
@@ -0,0 +1,10 @@
+package org.project.shop;
+
+public class GithubTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi gitHuB!");
+	}
+
+}
